c0011be7b82f27ba1ac7eebd0fdab387bf984c8f
Mathislu <37864606+Mathislu@users.noreply.github.com>
2021-11-19 11:19:41 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,25 @@
+document.write('\
+    <div id="header">\
+      <img id="kyle" src="yifengz.jpg"><br>\
+      <div id=title>\
+        Yifeng ZHENG<br/>\
+        <span>Assistant Professor</span><br/>\
+        <span class="address"><a href="http://cs.hitsz.edu.cn/" target="_blank">School of Computer Science and Technology</a></span><br/>\
+        <span class="address"><a href="http://www.hitsz.edu.cn/index.html" target="_blank">Harbin Institute of Technology (HIT)</a></span><br/>\<br/>\
+        <span class="address"><strong>Email:&nbsp;</strong>name DOT surname AT hit.edu.cn or my.cityu.edu.hk </span><br/><br/>\
+        <span class="address"><a href="https://scholar.google.com/citations?user=QtAO6K0AAAAJ&hl=en">Google Scholar</a></span><br/> \
+      </div>\
+\
+      <div id="bmenu-container">\
+        <ul class="bmenu">\
+          <li id="about"><a href="index.html">About</a></li><!--\
+          --><li id="publications"><a\
+          href="publications.html">Publications</a></li><!--\
+          --><li id="teaching"><a\
+          href="teaching.html">Teaching</a></li><!--\
+          --><li id="honors"><a href="honors.html">Honors</a></li><!--\
+          --><li id="service"><a href="service.html">Services</a></li>\
+        </ul>\
+      </div> <!-- bmenu-container -->\
+    </div> <!-- header -->\
+');
